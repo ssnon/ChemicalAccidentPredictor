e2513e42919e12d323a9f6f0699b82b6d0628ef4
ssnon <rldnr7959@gmail.com>
2026-05-11 14:19:56 +0900

    modify sh as LIME code changed

--- run_Analysis.sh
+++ run_Analysis.sh
@@ -1,30 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
+# JPype / KoNLPy libstdc++ ������ ��ȸ�ߴ� ������ �ʿ��ϸ� ����
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH:-}"
 
 DATA_DIR="dataset/"
+TRAIN_FILE="train/train.csv"
+VALID_FILE="valid/valid.csv"
 TEST_FILE="test/test.csv"
 
 MODEL_NAME="kobert"
-LRS=("4e-5" "3e-5" "2e-5" "1e-5")
+LRS=("3e-5")
 SEEDS=("42" "99" "123" "166" "1")
-WARMUP_RATIO=("0.0" "0.1" "0.2")
+WARMUP_RATIO=("0.2")
 SCHEDULER=("linear_warmup")
+
+mkdir -p logs
 
 for LR in "${LRS[@]}"; do
   for SEED in "${SEEDS[@]}"; do
     for SCHED in "${SCHEDULER[@]}"; do
       for WARMRATIO in "${WARMUP_RATIO[@]}"; do
         EXP_NAME="Aug1.3_kobert_lr_${LR}_scheduler_${SCHED}_warmupRatio_${WARMRATIO}_seed_${SEED}"
+        
+        echo "========================================"
+        echo "LIME: ${EXP_NAME}"
+        echo "========================================"
     
-        python lime_analysis_kobert.py \
+        python LIME_Analysis.py \
           --data_directory="$DATA_DIR" \
           --test_file="$TEST_FILE" \
           --model_directory="training_result/${EXP_NAME}" \
           --model="kobert" \
           --num_explain=100 \
-          --selection="first" \
+          --selection="all" \
           --lime_num_features=12 \
           --lime_num_samples=1000 \
           --seed="$SEED"
@@ -42,7 +51,7 @@ for MODEL_NAME in "${MODELS[@]}"; do
     echo "LIME: ${EXP_NAME}"
     echo "========================================"
 
-    python lime_analysis_classicML.py \
+    python LIME_Analysis.py \
       --data_directory="$DATA_DIR" \
       --test_file="$TEST_FILE" \
       --model_directory="training_result/${EXP_NAME}" \
